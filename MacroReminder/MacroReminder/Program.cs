ce9e522beecee6c6b3e66b9dc5e6aa58833ba9e3
Martin Liu <marty92@gmail.com>
2019-11-24 17:21:47 -0500

    adding red screen for macro reminder

---
+++ MacroReminder/MacroReminder/Program.cs
@@ -0,0 +1,151 @@
+﻿using NAudio.Wave;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+// http://www.pinvoke.net/default.aspx/gdi32.setdevicegammaramp
+
+public class Test
+{
+
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetDC(IntPtr hWnd);
+
+    [DllImport("gdi32.dll")]
+    public static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct RAMP
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Red;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Green;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Blue;
+    }
+
+
+
+    public static void SetGamma(bool redMode)
+    {
+
+        if(redMode)
+        {
+            int gamma = 256;
+            RAMP ramp = new RAMP();
+            ramp.Red = new ushort[256];
+            ramp.Green = new ushort[256];
+            ramp.Blue = new ushort[256];
+            for (int i = 1; i < 256; i++)
+            {
+                int iArrayValue = i * (gamma + 128);
+
+                if (iArrayValue > 65535)
+                    iArrayValue = 65535;
+
+
+                //    int iArrayValue2 = i * 128;// i * (0 + 128);
+
+                int iArrayValue2 = i * (0 + 128);
+
+                if (iArrayValue2 > 65535)
+                    iArrayValue2 = 65535;
+
+                ramp.Red[i] = (ushort)iArrayValue;
+
+                // ramp.Blue[i] = (ushort)iArrayValue;
+                // ramp.Green[i] = (ushort)iArrayValue2;
+                ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue2;
+
+
+                //                ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue2;
+            }
+            bool b = SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref ramp);
+            if (b)
+            {
+                // Console.WriteLine("SetDeviceGammaRamp is true");
+            }
+            else
+            {
+                Console.WriteLine("SetDeviceGammaRamp is false");
+            }
+        }
+        else
+        {
+            RAMP ramp = new RAMP();
+            ramp.Red = new ushort[256];
+            ramp.Green = new ushort[256];
+            ramp.Blue = new ushort[256];
+            int gamma = 100;
+            for (int i = 1; i < 256; i++)
+            {
+                int iArrayValue = i * (gamma + 128);
+
+                if (iArrayValue > 65535)
+                    iArrayValue = 65535;
+                ramp.Red[i] = ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue;
+            }
+            bool b = SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref ramp);
+            if (b)
+            {
+                // Console.WriteLine("SetDeviceGammaRamp is true");
+            }
+            else
+            {
+                Console.WriteLine("SetDeviceGammaRamp is false");
+            }
+        }
+    }
+
+
+    
+
+
+    public static void Main(string[] args)
+    {
+        string ent = "";
+        int g = 0;
+
+        WaveOutEvent waveOut = new WaveOutEvent();
+        WaveFileReader wavReader = new WaveFileReader("media.io_reminder.wav");
+        waveOut.Init(wavReader);
+
+
+        bool running = true;
+        bool displaying = false;
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        
+        long start = stopwatch.ElapsedMilliseconds;
+        long displayingStart = 0;
+        while (running)
+        {
+            long now = stopwatch.ElapsedMilliseconds;
+            if (now - start > 5000)
+            {
+                SetGamma(true);
+                wavReader.Position = 0;
+                waveOut.Play();
+
+                displaying = true;
+                start = now;
+                displayingStart = now;
+            }
+
+            if (displaying)
+            {
+                if(now - displayingStart > 1000)
+                {
+                    SetGamma(false);
+                    displaying = false;
+                }
+            }
+
+            Thread.Sleep(10);
+        }
+        
+    }
+}
--- macroReminder.py
+++ macroReminder.py
@@ -1,19 +1,111 @@
 import winsound
 import time
+from PIL import Image
+import pygame
+from pygame.locals import *
+import threading
+
+'''
+def soundFunc():
 
 
+    while True:
+        if(music):
+            # pygame.mixer.music.load('media.io_reminder.wav')
+            #pygame.mixer.music.play(0)
+            winsound.PlaySound('media.io_reminder.wav', winsound.SND_FILENAME)
+            music = False
+        # time.sleep(3)
+'''
 def mainFunc():
-	
-	start = time.time()
-	print("hello")
+    
+    pygame.init()
 
-	while True:
-		cur = time.time()
-		if cur - start > 5:
-#			winsound.PlaySound('Alarm-tone_short.wav', winsound.SND_FILENAME)
-			winsound.PlaySound('media.io_reminder.wav', winsound.SND_FILENAME)
-			start = time.time()
 
+    screen = pygame.display.set_mode([1280, 800], 0, 32)
+    #initiates screen
+
+#    sound = pygame.mixer.Sound('media.io_reminder.wav')
+    start = time.time()
+    # imageStart = time.time()
+    global displaying
+    global music
+
+    displaying = False 
+    music = False
+
+    # sound_thread = threading.Thread(target = soundFunc)
+    #sound_thread.start()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+        cur = time.time()
+        if cur - start > 5:
+#           winsound.PlaySound('Alarm-tone_short.wav', winsound.SND_FILENAME)
+            winsound.PlaySound('media.io_reminder.wav', winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_ALIAS)
+            
+#            sound.play()
+#           winsound.PlaySound('1_second_tone.wav', winsound.SND_FILENAME)
+#            image.show()
+
+
+
+            music = True
+            displaying = True
+            start = time.time()
+
+
+        if displaying:
+            if cur - start > 0.1:
+                displaying = False
+
+
+        screen.fill([255, 255, 255])
+        #screen is filled with a black background
+
+        if(displaying):
+            screen.fill([255, 0, 0])
+            #screen.blit(image1, [0, 0]) 
+            #here image1 is blitted onto screen at the coordinates (200,200)
+
+        #image1.blit(image2, [0, 0])
+        #here image2 is blitted onto image1 at the coordinates (0,0) which starts at the upper left of image1
+
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+        #updates display, which you can just ignore
+"""
+
+    start = time.time()
+    imageStart = time.time()
+
+    image = Image.open('scv_and_depo_red.png')
+    blankImage = Image.open('blank.png')
+    blankImage.show()
+    print("hello")
+    displaying = False
+
+
+    while True:
+        cur = time.time()
+
+        if cur - start > 5:
+#           winsound.PlaySound('Alarm-tone_short.wav', winsound.SND_FILENAME)
+            winsound.PlaySound('media.io_reminder.wav', winsound.SND_FILENAME)
+
+#           winsound.PlaySound('1_second_tone.wav', winsound.SND_FILENAME)
+            image.show()
+            displaying = True
+
+            start = time.time()
+
+        if displaying:
+            if cur - imageStart > 1:
+                displaying = False
+
+"""
 if __name__ == "__main__":
-	print ("Welcome to macroReminder")
-	mainFunc()
+    print ("Welcome to macroReminder")
+    mainFunc()
